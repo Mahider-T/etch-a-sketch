b266171a74282b83b5f596c775431c1fd75cc23d
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-05-06 11:47:43 +0300

    Add functions to mouse hover and on click of the button
    Two functions added
    One draws the sketchad after accepting the dimention of teh sketchpad from the user
    The other one (drawOnHover) is just the event listner on hover

--- myscript.js
+++ myscript.js
@@ -1,42 +1,105 @@
 const body = document.querySelector('body');
 
+const buttonContainer = document.createElement('div');//A container to hold the button
+buttonContainer.classList.add = "buttonContainer";
+buttonContainer.style.minWidth = "100vw"; //so that the button always is on top of the drawing board and not to the side 
+buttonContainer.style.minHeight = "fit-content";
+buttonContainer.style.display = "flex";
+buttonContainer.style.justifyContent = "center";
+buttonContainer.style.alignItems = "center";
+
+const button = document.createElement('button');//creates the button that prompts the number of boxes per row
+button.innerText = "How many boxes per row?";
+button.style.fontFamily = "Times New Roman";
+button.style.fontWeight = "bold";
+button.style.fontSize = "25px";
+button.style.color = "black";
+button.style.backgroundColor = "coral";
+button.style.width = "300px";
+button.style.height = "50px";
+button.style.borderRadius = "9px";
+buttonContainer.appendChild(button); //add the polished button into the container
+
 body.style.display = "flex";
 body.style.height = "100vh"; //to center the container div on the screen
+body.style.backgroundColor = "bisque";
 body.style.justifyContent = "center";//to center the container div on the screen
 body.style.alignItems = "center";//to center the container div on the screen
+body.style.flexWrap = "wrap";
+body.appendChild(buttonContainer);
+
 
 const container = document.createElement('div');
+let containerDimention = 700;//container should not grow or shrink depending on the number of boxes
+
 container.classList.add('container');
 container.style.display = "flex";
-container.style.flex = "1";
+container.style.flex = "1 1 auto";
 container.style.flexWrap = "wrap";
-container.style.maxWidth = "900px"; //limit max width to limit only certain amount of squares on a row
+container.style.maxHeight = `${containerDimention}px`;
+container.style.maxWidth = `${containerDimention}px`; 
+container.style.minWidth = `${containerDimention}px`;
 
-let divs; //a new mini-square box is saved here, updated throughout the loop
-for (let i = 1; i <= 16 * 16; i++) {
-    divs = document.createElement('div');
-    divs.classList.add(`box${i}`); //to give each box different className
-    divs.style.height = "50px";
-    divs.style.width = "50px";
-    divs.style.border = "solid black 2px";
-    container.appendChild(divs);
+// changes the background color and border of each div to red when it is hovered over
+//takes a nodelist as a parameter (the result of querySelectorAll() function)
+function drawOnHover(boxes) {
+    boxes.forEach(element => {
+        addEventListener('mouseover', function (e) {
+            if (e.target.className == element.className) { //the box being hovered over == the box reached after iteration
+                element.style.border = `solid red 1px`;
+                element.style.backgroundColor = "red";
+                //console.log(e.target.className);
+            }
+        }, true)
+    })
 }
 
-body.appendChild(container);
-
-const boxes = document.querySelectorAll(`.container div`); //access the div inside "container" class using child selector
-boxes.forEach(element => {addEventListener('mouseover', function(e){
-    if (e.target.className == element.className ) {
-            element.style.border = "solid red 2px";
-            element.style.backgroundColor = "red";
-          
-        //console.log(e.target.className);
+//The function below draws the pad to be sketched over
+let divs; //a new mini-square box is saved here, updated throughout the loop
+function drawPad(boxPerRow){
+    let boxDimention = containerDimention/boxPerRow;
+    for (let i = 1; i <= boxPerRow * boxPerRow; i++) {
+        divs = document.createElement('div');
+        divs.classList.add(`box${i}`); //to give each box different className
+        divs.style.boxSizing = "border-box";
+        divs.style.height = `${boxDimention}px`;
+        divs.style.width = `${boxDimention}px`;
+        divs.style.border = `solid black 1px`;
+        container.appendChild(divs);//each div(box) is appended to the container after each iteration 
     }
-    
+    body.appendChild(container);
 
-}, true)
-    
-});
+}
+
+let initialNumberOfBoxesPerRow = 10;
+drawPad(initialNumberOfBoxesPerRow);
+const initialBoxes = document.querySelectorAll(`.container div`);
+drawOnHover(initialBoxes);
+
+button.addEventListener('click', () => {
+    numberOfBoxesPerRow = prompt("How many boxes in a row?");
+    while(!(numberOfBoxesPerRow > 0 && numberOfBoxesPerRow < 100)){//restrict entry between 1 - 100
+        alert("Only numbers 1 - 99 are allowed.");
+        numberOfBoxesPerRow = prompt("How many boxes in a row?");
+    }
+
+    if (numberOfBoxesPerRow > 0 && numberOfBoxesPerRow < 100) {
+        //document.body.removeChild(container);
+        //container.textContent = "";
+        // container.removeChild(container.lastChild);
+        while (container.firstChild) {// remove prior boxes
+            container.removeChild(container.firstChild);
+        }
+        drawPad(numberOfBoxesPerRow); 
+        const newBoxes = document.querySelectorAll(`.container div`)
+        drawOnHover(newBoxes);
+    }
+})
+
+
+
+
+
 
 
 
